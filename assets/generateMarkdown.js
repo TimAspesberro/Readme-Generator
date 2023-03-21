2fd886d9dc99cec402b2aa83b8ecda846a845b5b
Tim Aspesberro <taspes@gmail.com>
2023-03-21 19:31:26 -0400

    changes to generatemarkdown

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -10,16 +10,33 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseLink) {
+  if(!licenseLink){
+    return '';
+  }else if(licenseLink === 'Mozilla Public License 2.0'){
+    return 'https://www.mozilla.org/en-US/MPL/2.0/';
+  }else if(licenseLink === 'Apache License 2.0'){
+    return 'https://www.apache.org/licenses/LICENSE-2.0';
+  }else if(licenseLink === 'MIT License'){
+    return 'https://opensource.org/license/mit/';
+  }else if(licenseLink === 'Boost Software License 1.0'){
+    return 'https://www.boost.org/users/license.html';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license){
+    return '';
+  }else{
+    return license;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data}
 `;
 }
 
--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
 }
 
 // TODO: Create a function to initialize app
@@ -15,38 +17,38 @@ function init() {
     inquirer
   .prompt([
     {
-      type: 'input',
       name: 'title',
       message: 'What is the title of your project?',
+      type: 'input',
     },
     {
-      type: 'input',
       name: 'description',
       message: 'Describe the purpose of your application. What was your motivation and what problem does it solve? ',
+      type: 'input',
     },
     {
-      type: 'checkbox',
       name: 'license',
       message: 'Choose a license for you application',
       choices: ['Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0'],
+      type: 'list',
     },
     {
-      type: 'input',
       name: 'github',
       message: 'Please provide your GitHub username.',
+      type: 'input',
     },
     {
-      type: 'input',
       name: 'email',
       message: 'Please provide your email address.',
+      type: 'input',
     },
   ])
   .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+    const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+    let titlePrompt = data.title;
+    console.log(data.license);
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
+  
   });
 }
 
